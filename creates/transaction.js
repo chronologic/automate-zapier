8e12730356566f8bc2697d44c92572e021ec06af
Piotr Kosiński <piotrkosinski@gmail.com>
2019-01-04 12:43:09 +0100

    Return always decoded  tx. Better help texts

--- creates/transaction.js
+++ creates/transaction.js
@@ -1,12 +1,11 @@
-const ethers = require('ethers');
+const ethers = require("ethers");
 
-const SENT = "Sent";
+const MINED = "Mined";
 const WAITING = "Waiting for nonce";
 const NONCE_SPENT = "Nonce already spent";
-const ALREADY_SENT = "Transaction already sent";
 
 const executeTransaction = async (z, bundle) => {
-  z.console.log('Inputs', bundle.inputData);
+  z.console.log("Inputs", bundle.inputData);
 
   const signedTransaction = bundle.inputData.Transaction;
   const parsed = ethers.utils.parseTransaction(signedTransaction);
@@ -16,63 +15,63 @@ const executeTransaction = async (z, bundle) => {
   const currentNonce = await provider.getTransactionCount(parsed.from);
   const transactionNonce = parsed.nonce;
 
+  let state = MINED;
+  let result = parsed;
+
   if (transactionNonce < currentNonce) {
-    return { transaction: signedTransaction, state: NONCE_SPENT} 
+    state = NONCE_SPENT;
   } else if (transactionNonce > currentNonce) {
-    return { transaction: signedTransaction, state: WAITING }
+    state = WAITING;
+  } else {
+    const response = await provider.sendTransaction(signedTransaction);
+
+    result = await response.wait(3);
+    state = MINED;
   }
 
-  const response = await provider.sendTransaction(signedTransaction);
-  const receipt = await response.wait(3);
-
-  receipt.state = SENT;
-
-  return receipt;
+  return { ...result, state };
 };
 
 const Transaction = {
-  key: 'transaction',
-  noun: 'Transaction',
+  key: "transaction",
+  noun: "Transaction",
   display: {
-    label: 'Execute Transaction',
-    description: 'Executes your transaction'
+    label: "Execute Ethereum Transaction",
+    description: "Executes your signed Ethereum transaction using one of the public nodes."
   },
   operation: {
     inputFields: [
-      {key: 'Transaction', required: true, type: 'string', helpText: 'Signed transaction'}
+      {
+        key: "Transaction",
+        required: true,
+        type: "string",
+        helpText: "Signed Ethereum transaction. Automate expects correctly signed Ethereum transaction, please use one of the available Ethereum wallets for e.g https://www.myetherwallet.com/#offline-transaction "
+      }
     ],
     perform: executeTransaction,
-
-    // In cases where Zapier needs to show an example record to the user, but we are unable to get a live example
-    // from the API, Zapier will fallback to this hard-coded sample. It should reflect the data structure of
-    // returned records, and have obviously dummy values that we can show to any user.
     sample: {
-      transaction: '0x',
-      hash: '0x96f9333d012fb7fa4d60338b590f4006d68f9a573d1780d8c4e8a47520892804',
-      to: '0xe4e9dde021ef7dd34b551ec975ef38135ce44ff6',
-      from: '0x94f0f15864cbb346469ab5ef1a07a0a5351effd8',
+      transaction: "0x",
+      hash:
+        "0x96f9333d012fb7fa4d60338b590f4006d68f9a573d1780d8c4e8a47520892804",
+      to: "0xe4e9dde021ef7dd34b551ec975ef38135ce44ff6",
+      from: "0x94f0f15864cbb346469ab5ef1a07a0a5351effd8",
       nonce: 7,
-      gasLimit: '40002',
-      gasPrice: '1000000000',
-      data: '',
-      value: '1000000000000000000',
+      gasLimit: "40002",
+      gasPrice: "1000000000",
+      data: "",
+      value: "1000000000000000000",
       chainId: 1,
       status: 1,
-      state: SENT
+      state: MINED
     },
-
-    // If the resource can have fields that are custom on a per-user basis, define a function to fetch the custom
-    // field definitions. The result will be used to augment the sample.
-    // outputFields: () => { return []; }
-    // Alternatively, a static field definition should be provided, to specify labels for the fields
     outputFields: [
-      {key: 'state', label: 'Execution state'},
-      {key: 'Transaction', label: 'Signed transaction'},
-      {key: 'hash', label: 'Transaction hash'},
-      {key: 'to', label: 'Transaction recipient'},
-      {key: 'from', label: 'Transaction sender'},
-      {key: 'nonce', label: 'Transaction nonce'},
-      {key: 'gasLimit', label: 'Gas limit'}
+      { key: "state", label: "Execution state" },
+      { key: "Transaction", label: "Signed transaction" },
+      { key: "hash", label: "Transaction hash" },
+      { key: "to", label: "Transaction recipient" },
+      { key: "from", label: "Transaction sender" },
+      { key: "nonce", label: "Transaction nonce" },
+      { key: "gasLimit", label: "Gas limit" }
     ]
   }
 };
