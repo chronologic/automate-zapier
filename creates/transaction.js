32e74381e1e3bb403bb55c475b0bbff9e172d1e4
Piotr Kosiński <piotrkosinski@gmail.com>
2019-01-14 09:14:00 +0100

    trimming/raw detection. better error messages

--- creates/transaction.js
+++ creates/transaction.js
@@ -6,33 +6,71 @@ const STATE = {
   TEST: "TEST"
 };
 
+const Error = {
+  NOT_COMPATIBLE: "This transaction seems to be not compatible with Ethereum transaction, please make sure you signed and copied it correctly so it looks like 0xf86c588509502f...",
+  RAW: "This transaction looks like RAW unsigned transaction, please make sure you signed and copied it correctly so it looks like 0xf86c588509502f...",
+  UNSIGNED: "The transaction format looks good but it seems to be unsigned, please make sure you signed and copied it correctly so it looks like 0xf86c588509502f...",
+  NOT_SUPPORTED: "The network id you set is unfortunately not yet supported by Automate. The list of supported chains is Ethereum Mainnet, Ropsten, Kovan, Rinkeby."
+}
+
+const proto = {
+  hash: "",
+  to: "",
+  from: "",
+  nonce: "",
+  gasLimit: "",
+  gasPrice: "",
+  data: "",
+  value: "",
+  chainId: 1,
+  status: 1,
+  contractAddress: "",
+  transactionIndex: 0,
+  root: "",
+  gasUsed: 0,
+  logsBloom: "",
+  blockHash: "",
+  transactionHash: "",
+  logs: [{ transactionHash: "", logIndex: 0, topics: ["", ""] }],
+  blockNumber: 0,
+  confirmations: 0,
+  cumulativeGasUsed: 0,
+  byzantium: true,
+  state: STATE.TEST
+};
+
 const validateTransaction = tx => {
   let parsed;
   try {
+    tx = tx.trim();
     parsed = ethers.utils.parseTransaction(tx);
-  } catch(err) {
-    throw new Error("Not compatible Ethereum transaction");
+  } catch (err) {
+    const raw = JSON.parse(tx);
+    if (raw.nonce) {
+      throw new Error(Error.RAW);
+    }
+    throw new Error(Error.NOT_COMPATIBLE);
   }
-  
+
   if (!parsed.from) {
-    throw new Error("Transaction not signed");
+    throw new Error(Error.UNSIGNED);
   }
   try {
-    ethers.utils.getNetwork(parsed.chainId)
-  } catch(err) {
-    throw new Error("Unsupported network id = " + parsed.chainId)
+    ethers.utils.getNetwork(parsed.chainId);
+  } catch (err) {
+    throw new Error(Error.NOT_SUPPORTED);
   }
 
   return parsed;
 };
 
 const humanize = result => {
-  for(const key in result) {
+  for (const key in result) {
     if (result[key] instanceof ethers.utils.BigNumber) {
       result[key] = result[key].toString();
     }
   }
-}
+};
 
 const executeTransaction = async (z, bundle) => {
   z.console.log("Inputs", bundle.inputData);
@@ -45,7 +83,7 @@ const executeTransaction = async (z, bundle) => {
   const senderNonce = await provider.getTransactionCount(parsed.from);
 
   let state = STATE.MINED;
-  let result = parsed;
+  let result = { ...proto, ...parsed };
 
   if (bundle.meta.frontend) {
     state = STATE.TEST;
@@ -82,7 +120,6 @@ const Transaction = {
     ],
     perform: executeTransaction,
     sample: {
-      transaction: "0x",
       hash:
         "0x96f9333d012fb7fa4d60338b590f4006d68f9a573d1780d8c4e8a47520892804",
       to: "0xe4e9dde021ef7dd34b551ec975ef38135ce44ff6",
@@ -94,17 +131,29 @@ const Transaction = {
       value: "1000000000000000000",
       chainId: 1,
       status: 1,
+      contractAddress: "0x1234",
+      transactionIndex: 140,
+      root: "0x4567",
+      gasUsed: 100000,
+      logsBloom: "0x223231231",
+      blockHash:
+        "0x40bc237b3902b94ae7e2d7fc3eed17c6e08ccb4e6a72797b3715a2ec24773506",
+      transactionHash:
+        "0x96f9333d012fb7fa4d60338b590f4006d68f9a573d1780d8c4e8a47520892804",
+      logs: [
+        {
+          transactionHash:
+            "0x96f9333d012fb7fa4d60338b590f4006d68f9a573d1780d8c4e8a47520892804",
+          logIndex: 0,
+          topics: ["0x1234", "0x2423"]
+        }
+      ],
+      blockNumber: 100000,
+      confirmations: 3,
+      cumulativeGasUsed: 1000000,
+      byzantium: true,
       state: STATE.MINED
-    },
-    outputFields: [
-      { key: "state", label: "Execution state" },
-      { key: "Transaction", label: "Signed transaction" },
-      { key: "hash", label: "Transaction hash" },
-      { key: "to", label: "Transaction recipient" },
-      { key: "from", label: "Transaction sender" },
-      { key: "nonce", label: "Transaction nonce" },
-      { key: "gasLimit", label: "Gas limit" }
-    ]
+    }
   }
 };
 
--- test/index.js
+++ test/index.js
@@ -1,35 +1,53 @@
-const assert = require('assert');
-const zapier = require('zapier-platform-core');
+const assert = require("assert");
+const zapier = require("zapier-platform-core");
 
 // Use this to make test calls into your app:
-const App = require('../index');
+const App = require("../index");
 const appTester = zapier.createAppTester(App);
 
-describe('My App', () => {
-
-  it('should throw when not compatible transaction', async() => {
+describe("My App", () => {
+  it("should throw when not compatible transaction", async () => {
     const bundle = {
       inputData: {
-        Transaction: '0xbadtransaction'
+        Transaction: "0xbadtransaction"
       }
-    }
+    };
 
-    await assert.rejects(appTester(App.creates.transaction.operation.perform, bundle), "Not compatible Ethereum transaction");
+    await assert.rejects(
+      appTester(App.creates.transaction.operation.perform, bundle),
+      Error.NOT_COMPATIBLE
+    );
   });
-  it('should humanize the results', async() => {
+  it("should humanize the results", async () => {
     const bundle = {
       inputData: {
-        Transaction: "0xf86b578509502f900082520894eac4cf1e68f0d81b1abbce4152f2ef73b3f8f6ee872386f26fc100008078a0a7ede4b9016c810864f286d40e7a99eaf9bf2905fc6c65a1b3ee2dc8b4cb518aa06bb07576aa1759f838f4522b77e8b7dc20090d340bdc3d67a94ccc66f1aa4e69",
+        Transaction:
+          "0xf86b578509502f900082520894eac4cf1e68f0d81b1abbce4152f2ef73b3f8f6ee872386f26fc100008078a0a7ede4b9016c810864f286d40e7a99eaf9bf2905fc6c65a1b3ee2dc8b4cb518aa06bb07576aa1759f838f4522b77e8b7dc20090d340bdc3d67a94ccc66f1aa4e69"
       },
-      meta: {
-          
-      }
-    }
+      meta: {}
+    };
 
-    const result = await appTester(App.creates.transaction.operation.perform, bundle);
+    const result = await appTester(
+      App.creates.transaction.operation.perform,
+      bundle
+    );
 
     assert.equal(result.gasLimit, 21000);
     assert.equal(result.gasPrice, 40000000000);
     assert.equal(result.value, 10000000000000000);
-  })
+  });
+
+  it("should trim transaction", async () => {
+    const bundle = {
+      inputData: {
+        Transaction:
+          " 0xf86b578509502f900082520894eac4cf1e68f0d81b1abbce4152f2ef73b3f8f6ee872386f26fc100008078a0a7ede4b9016c810864f286d40e7a99eaf9bf2905fc6c65a1b3ee2dc8b4cb518aa06bb07576aa1759f838f4522b77e8b7dc20090d340bdc3d67a94ccc66f1aa4e69      "
+      },
+      meta: {}
+    };
+
+    assert.ok(
+      await appTester(App.creates.transaction.operation.perform, bundle)
+    );
+  });
 });
